acf90e03e2e127ea3a310f0d83f82d5e258d2510
Juan Tenezaca <tenj34@gmail.com>
2018-09-19 21:26:33 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -17,7 +17,7 @@ def select_value_and_count_of_most_prolific_species
      FROM characters
      GROUP BY characters.species
      ORDER BY COUNT(characters.species) DESC LIMIT 1;"
-   end
+end
 
 def select_name_and_series_subgenres_of_authors
 end
