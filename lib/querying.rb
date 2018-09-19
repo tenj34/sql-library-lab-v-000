9870b7bedd325c70e6d3093c56abfcb61d39ade6
Juan Tenezaca <tenj34@gmail.com>
2018-09-19 21:28:21 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -27,6 +27,12 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
+  "SELECT series.title
+ FROM series
+ INNER JOIN characters ON series.id = characters.series_id
+ GROUP BY series.title
+ HAVING characters.species = 'human'
+ ORDER BY COUNT(characters.species) DESC LIMIT 1;"
 end
 
 def select_character_names_and_number_of_books_they_are_in
