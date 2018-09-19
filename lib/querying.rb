73062b89b8a8a766905f49c2093c0b6ed1aa9710
Juan Tenezaca <tenj34@gmail.com>
2018-09-19 21:25:57 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -13,17 +13,13 @@ end
 
 
 def select_value_and_count_of_most_prolific_species
-  "Write your SQL query here"
 end
 
 def select_name_and_series_subgenres_of_authors
-  "Write your SQL query here"
 end
 
 def select_series_title_with_most_human_characters
-  "Write your SQL query here"
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "Write your SQL query here"
 end
