31b8cc5fcf0add0f9d984f6747b4fb79b73e826f
Juan Tenezaca <tenj34@gmail.com>
2018-09-19 21:28:11 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -20,6 +20,10 @@ def select_value_and_count_of_most_prolific_species
 end
 
 def select_name_and_series_subgenres_of_authors
+  "SELECT authors.name, subgenres.name
+   FROM series, authors, subgenres
+   WHERE  authors.id = series.author_id AND series.subgenre_id = subgenres.id
+   GROUP BY authors.name;"
 end
 
 def select_series_title_with_most_human_characters
