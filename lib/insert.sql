05af4af7a0197fd8b0ab414c623be850b230d02e
Juan Tenezaca <tenj34@gmail.com>
2018-09-19 21:13:51 +0000

    automatically backed up by learn

---
+++ lib/insert.sql
@@ -0,0 +1,16 @@
+INSERT INTO series (title, author_id, subgenre_id) VALUES ("Game of Thrones", 1, 1);
+INSERT INTO series (title, author_id, subgenre_id) VALUES ("Harry Potter", 2, 2);
+INSERT INTO books (title, year, series_id) VALUES ("The Prisoner of Azkaban", 1999, 2);
+INSERT INTO books (title, year, series_id) VALUES ("A Feast of Crows", 2005, 1);
+INSERT INTO books (title, year, series_id) VALUES ("The Sorcerer's Stone", 1997, 2);
+INSERT INTO books (title, year, series_id) VALUES ("The Deathly Hallows", 2007, 2);
+INSERT INTO books (title, year, series_id) VALUES ("A Clash of Kings", 1998, 1);
+INSERT INTO books (title, year, series_id) VALUES ("A Game of Thrones", 1996, 1);
+INSERT INTO characters (name, motto, species, author_id, series_id) VALUES ("Harry Potter", "", "Human", 1, 1);
+INSERT INTO characters (name, motto, species, author_id, series_id) VALUES ("Hermione Granger", "", "Human", 1, 1);
+INSERT INTO characters (name, motto, species, author_id, series_id) VALUES ("Jon Snow", "", "Human", 1, 1);
+INSERT INTO characters (name, motto, species, author_id, series_id) VALUES ("The Hound", "", "Human", 1, 1);
+INSERT INTO characters (name, motto, species, author_id, series_id) VALUES ("Ron Weasely", "", "Human", 1, 1);
+INSERT INTO characters (name, motto, species, author_id, series_id) VALUES ("", "", "Human", 1, 1);
+INSERT INTO characters (name, motto, species, author_id, series_id) VALUES ("", "", "Human", 1, 1);
+INSERT INTO characters (name, motto, species, author_id, series_id) VALUES ("", "", "Human", 1, 1);
